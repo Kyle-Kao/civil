3078544fe494854be2c9c2d0edd032af478fd836
Kyle-KAO <kyle.kao@aimestechcp.com>
2025-03-18 11:39:09 +0800

    Updates

---
+++ assets/About-DFEPiu1S.js
@@ -0,0 +1,9 @@
+var o={exports:{}},e={};/**
+ * @license React
+ * react-jsx-runtime.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var i;function v(){if(i)return e;i=1;var l=Symbol.for("react.transitional.element"),R=Symbol.for("react.fragment");function s(d,r,t){var u=null;if(t!==void 0&&(u=""+t),r.key!==void 0&&(u=""+r.key),"key"in r){t={};for(var n in r)n!=="key"&&(t[n]=r[n])}else t=r;return r=t.ref,{$$typeof:l,type:d,key:u,ref:r!==void 0?r:null,props:t}}return e.Fragment=R,e.jsx=s,e.jsxs=s,e}var a;function p(){return a||(a=1,o.exports=v()),o.exports}var x=p();function _(){return x.jsx(x.Fragment,{children:"這是關於"})}const f=Object.freeze(Object.defineProperty({__proto__:null,default:_},Symbol.toStringTag,{value:"Module"}));export{f as A,x as j};
---
+++ assets/Home-aXj8suSk.js
@@ -0,0 +1,1 @@
+import{j as r}from"./About-DFEPiu1S.js";function e(){return r.jsx(r.Fragment,{children:"這是首頁"})}export{e as default};
